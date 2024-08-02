7a7696b4f0a75c93bc38811b1796c96af1a55427
Alex Claridge <thexjprojectac@gmail.com>
2024-08-02 11:26:30 +0100

    Added Current Project Progress to Git

---
+++ The-Pond-No-Peht/Assets/Scripts/Player/Player_Movement.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player_Movement : MonoBehaviour
+{
+    public float moveSpeed = 3.0f;
+    public PlayerController playerControls;
+
+    Rigidbody2D rb;
+    Vector2 moveDirection = Vector2.zero;
+    InputAction move;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        playerControls = new PlayerController();
+    }
+
+    private void OnEnable()
+    {
+        move = playerControls.Player.Move;
+        move.Enable();
+    }
+
+    private void OnDisable()
+    {
+        move.Disable();
+    }
+
+    void Update()
+    {
+        moveDirection = move.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+    }
+}
