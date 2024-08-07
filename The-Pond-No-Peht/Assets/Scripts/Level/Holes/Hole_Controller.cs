fc095909dc217faaf7a007d234c229bd88c74108
Alex Claridge <thexjprojectac@gmail.com>
2024-08-07 14:34:38 +0100

    Fixed Hole Bugs

--- The-Pond-No-Peht/Assets/Scripts/Level/Holes/Hole_Controller.cs
+++ The-Pond-No-Peht/Assets/Scripts/Level/Holes/Hole_Controller.cs
@@ -14,11 +14,15 @@ public class Hole_Controller : MonoBehaviour
     int amountInHole;
     Vector2[] baseCoords = new Vector2[4];
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("collided with player");
+    }
     private void OnTriggerStay2D(Collider2D collision)
     {
+        Debug.Log("Staying with player");
         if (collision.CompareTag("Player") && accessPlayerStats.playerLevel == holeLevel)
         {
-            Debug.Log("collided with player");
             baseCoords[0] = playerBasePosition.position + new Vector3(0, 0.5f * playerBasePosition.lossyScale.y, 0);
             baseCoords[1] = playerBasePosition.position - new Vector3(0, 0.5f * playerBasePosition.lossyScale.y, 0);
             baseCoords[2] = playerBasePosition.position + new Vector3(0.5f * playerBasePosition.lossyScale.x, 0, 0);
@@ -27,28 +31,28 @@ public class Hole_Controller : MonoBehaviour
             amountInHole = 0;
             for (int i = 0; i < 4; i++)
             {
-                if (holeArea.bounds.Contains(baseCoords[i])) { amountInHole++; }
+                if (holeArea.OverlapPoint(baseCoords[i])) { amountInHole++; }
             }
             if (amountInHole == 4) 
             { 
-                playerInThisHole = true; 
-                accessPlayerStats.playerLevel = -10;
-                accessPlayerStats.playerOnSurface = false;
+                playerInThisHole = true;
+                accessPlayerStats.playerAboveSurface = false;
             }
             else 
             { 
                 playerInThisHole = false;
-                accessPlayerStats.playerLevel = holeLevel;
+                accessPlayerStats.playerAboveSurface = true;
             }
         }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
+        Debug.Log("Exited with player");
         if (collision.CompareTag("Player"))
         {
             playerInThisHole = false;
-            accessPlayerStats.playerLevel = holeLevel;
+            accessPlayerStats.playerAboveSurface = true;
         }
     }
 }
--- The-Pond-No-Peht/Assets/Scripts/Player/Player_Controller.cs
+++ The-Pond-No-Peht/Assets/Scripts/Player/Player_Controller.cs
@@ -13,14 +13,16 @@ public class Player_Controller : MonoBehaviour
     public float slightFloatBoundary = 1f;
     public float slightFloatReduction = 0.2f;
     public bool playerOnSurface = true;
+    public bool playerAboveSurface = true;
 
     public InputAction move;
     public InputAction jump;
 
-    float recordHeight = 0f;
+    //float recordHeight = 0f;
 
     private void FixedUpdate()
     {
+        if (!playerAboveSurface) { playerOnSurface = false; }
         if (!playerOnSurface)
         {
             playerHeight += playerVerticalVelocity * Time.deltaTime;
@@ -31,12 +33,12 @@ public class Player_Controller : MonoBehaviour
         {
             playerVerticalVelocity = 0f;
         }
-        if (playerHeight <= playerLevel)
+        if (playerHeight <= playerLevel && playerAboveSurface)
         {
             playerHeight = playerLevel;
             playerOnSurface = true;
         }
-        if (recordHeight > playerHeight) { Debug.Log(recordHeight); }
-        if (recordHeight < playerHeight) { recordHeight = playerHeight; }
+        //if (recordHeight > playerHeight) { Debug.Log(recordHeight); }
+        //if (recordHeight < playerHeight) { recordHeight = playerHeight; }
     }
 }
